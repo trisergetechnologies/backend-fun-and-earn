778250abf45e66455896e2f7cffeb33afb117ae7
Vaibhav Pandey <vaibhavkpandey7@gmail.com>
2025-07-14 11:36:35 +0530

    d

--- src/controllers/auth.controllers.js
+++ src/controllers/auth.controllers.js
@@ -113,7 +113,9 @@ exports.register = async (req, res) => {
           id: newUser._id,
           name: newUser.name,
           role: newUser.role,
-          applications: newUser.applications
+          applications: newUser.applications,
+          phone: user.phone,
+          referralCode: referralCode,
         }
       }
     });
@@ -187,7 +189,9 @@ exports.login = async (req, res) => {
           id: user._id,
           name: user.name,
           role: user.role,
-          applications: user.applications
+          applications: user.applications,
+          phone: user.phone,
+          referralCode: referralCode,
         }
       }
     });
