484a48e8e7aab06c00cce6acbad9d5e10838e5af
Vaibhav Pandey <vaibhavkpandey7@gmail.com>
2025-07-17 02:15:30 +0530

    adf

--- src/eCart/routes/user/general.routes.user.js
+++ src/eCart/routes/user/general.routes.user.js
@@ -1,8 +1,9 @@
 const Express = require("express");
-const { updateProfile, getUserProfile, addBankDetails, updateBankDetails, deleteBankDetails, getRewards } = require("../../controllers/user/general.controller.user");
+const { updateProfile, getUserProfile, addBankDetails, updateBankDetails, deleteBankDetails, getRewards, changePassword } = require("../../controllers/user/general.controller.user");
 const userGeneralRouter = Express.Router()
 
 userGeneralRouter.get('/getprofile', getUserProfile);
+userGeneralRouter.get('/changepassword', changePassword);
 userGeneralRouter.patch('/updateprofile', updateProfile);
 userGeneralRouter.post('/addbankdetails', addBankDetails);
 userGeneralRouter.patch('/updatebankdetails', updateBankDetails);
