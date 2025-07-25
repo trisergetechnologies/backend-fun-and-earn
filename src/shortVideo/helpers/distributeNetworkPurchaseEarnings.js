24baaa5aa3f95c4f64d7294ba1bc6a829c24b834
Vaibhav Pandey <vaibhavkpandey7@gmail.com>
2025-07-26 03:16:25 +0530

    buffer

--- src/shortVideo/helpers/distributeNetworkPurchaseEarnings.js
+++ src/shortVideo/helpers/distributeNetworkPurchaseEarnings.js
@@ -19,7 +19,7 @@ exports.distributeNetworkPurchaseEarnings = async (newUser) => {
       if (Math.abs(user.serialNumber - buyerSerial) <= maxRange && user._id.toString() !== newUser._id.toString()) {
         const amount = 0.01 * buyerPackagePrice;
 
-        user.wallets.shortVideoWallet += amount;
+        user.wallets.shortVideoWallet = (user.wallets.shortVideoWallet || 0) + amount;
 
         await Promise.all([
           new EarningLog({
