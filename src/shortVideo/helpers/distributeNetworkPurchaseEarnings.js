b202127a440c26d8afe9cd3fa3446193d35032a6
Vaibhav Pandey <vaibhavkpandey7@gmail.com>
2025-07-26 12:52:59 +0530

    ad

--- src/shortVideo/helpers/distributeNetworkPurchaseEarnings.js
+++ src/shortVideo/helpers/distributeNetworkPurchaseEarnings.js
@@ -2,8 +2,47 @@ const Package = require('../../models/Package');
 const User = require('../../models/User');
 const EarningLog = require('../models/EarningLog');
 
+// exports.distributeNetworkPurchaseEarnings = async (newUser) => {
+//   try {
+//     const allUsers = await User.find({ serialNumber: { $ne: null } })
+//       .select('_id serialNumber package wallets')
+//       .sort({ serialNumber: 1 })
+//       .populate('package');
+
+//     const buyerSerial = newUser.serialNumber;
+//     const buyerPackagePrice = newUser.package.price;
+
+//     for (const user of allUsers) {
+//       if (!user.package) continue;
+
+//       const maxRange = user.package.name === 'Diamond' ? 20 : 10;
+
+//       if (Math.abs(user.serialNumber - buyerSerial) <= maxRange && user._id.toString() !== newUser._id.toString()) {
+//         const amount = 0.01 * buyerPackagePrice;
+
+//         user.wallets.shortVideoWallet = Number(user.wallets.shortVideoWallet || 0) + amount;
+
+//           const earningLog = EarningLog({
+//             userId: user._id,
+//             source: 'networkPurchase',
+//             fromUser: newUser._id,
+//             amount,
+//           }).save();
+
+//           earningLog.save();
+//           awaituser.save();
+//       }
+//     }
+//   } catch (err) {
+//     console.error('Error in distributeNetworkPurchaseEarnings:', err);
+//   }
+// };
+
 exports.distributeNetworkPurchaseEarnings = async (newUser) => {
   try {
+
+    console.log("came into dis.in.Netwo...new User came",newUser);
+
     const allUsers = await User.find({ serialNumber: { $ne: null } })
       .select('_id serialNumber package wallets')
       .sort({ serialNumber: 1 })
@@ -13,24 +52,23 @@ exports.distributeNetworkPurchaseEarnings = async (newUser) => {
     const buyerPackagePrice = newUser.package.price;
 
     for (const user of allUsers) {
-      if (!user.package) continue;
+      if (!user.package || user._id.equals(newUser._id)) continue;
 
       const maxRange = user.package.name === 'Diamond' ? 20 : 10;
+      const isInRange = Math.abs(user.serialNumber - buyerSerial) <= maxRange;
 
-      if (Math.abs(user.serialNumber - buyerSerial) <= maxRange && user._id.toString() !== newUser._id.toString()) {
+      if (isInRange) {
         const amount = 0.01 * buyerPackagePrice;
-
         user.wallets.shortVideoWallet = Number(user.wallets.shortVideoWallet || 0) + amount;
 
-          const earningLog = EarningLog({
-            userId: user._id,
-            source: 'networkPurchase',
-            fromUser: newUser._id,
-            amount,
-          }).save();
-
-          earningLog.save();
-          awaituser.save();
+        await EarningLog.create({
+          userId: user._id,
+          source: 'networkPurchase',
+          fromUser: newUser._id,
+          amount,
+        });
+        
+        await user.save();
       }
     }
   } catch (err) {
