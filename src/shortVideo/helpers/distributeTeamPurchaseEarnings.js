cd33f3ff118b15a4a4649689708768d5a78d74c4
Vaibhav Pandey <vaibhavkpandey7@gmail.com>
2025-07-27 01:57:43 +0530

    adsd

--- src/shortVideo/helpers/distributeTeamPurchaseEarnings.js
+++ src/shortVideo/helpers/distributeTeamPurchaseEarnings.js
@@ -2,7 +2,7 @@ const User = require('../../models/User');
 const EarningLog = require('../models/EarningLog');
 const Package = require('../../models/Package');
 
-const TEAM_PURCHASE_PERCENTAGES = [25, 10, 5, 4, 3, 3, 3, 3, 2, 2];
+const TEAM_PURCHASE_PERCENTAGES = [25, 10, 5, 4, 3, 3, 3, 3, 2, 2]; // Level 1 to 10
 
 exports.distributeTeamPurchaseEarnings = async (userId, packagePrice) => {
   try {
@@ -12,34 +12,39 @@ exports.distributeTeamPurchaseEarnings = async (userId, packagePrice) => {
     let currentReferral = currentUser.referredBy;
     let level = 0;
 
+    // Traverse up to 10 levels
     while (currentReferral && level < 10) {
       const referrer = await User.findOne({ referralCode: currentReferral }).populate('package');
       if (!referrer || !referrer.package) break;
 
-      const maxLevel = referrer.package.name === 'Diamond' ? 10 : 5;
-      console.log("checking if undefined",referrer.package.name);
-      console.log("what's the max level", maxLevel);
-      if (level >= maxLevel) break;
+      const maxEarningLevel = referrer.package.name === 'Diamond' ? 10 : 5;
 
-      const percent = TEAM_PURCHASE_PERCENTAGES[level];
-      const earningAmount = +(packagePrice * (percent / 100)).toFixed(2);
+      if (level < maxEarningLevel) {
+        const percent = TEAM_PURCHASE_PERCENTAGES[level];
+        const earningAmount = +(packagePrice * (percent / 100)).toFixed(2);
 
-      referrer.wallets.shortVideoWallet += earningAmount;
+        // Add earnings to wallet
+        referrer.wallets.shortVideoWallet += earningAmount;
 
-      await Promise.all([
-        new EarningLog({
-          userId: referrer._id,
-          source: 'teamPurchase',
-          fromUser: userId,
-          amount: earningAmount
-        }).save(),
+        await Promise.all([
+          new EarningLog({
+            userId: referrer._id,
+            type: 'team',
+            source: 'purchase',
+            level: level + 1,
+            earnedFrom: userId,
+            amount: earningAmount
+          }).save(),
 
-        referrer.save()
-      ]);
+          referrer.save()
+        ]);
+      }
 
+      // Move to next level up in referral chain
       currentReferral = referrer.referredBy;
       level++;
     }
+
   } catch (err) {
     console.error('Error in distributeTeamPurchaseEarnings:', err);
   }
