9e7c66eda8f4bc3ffa79c66270f8f3c4a1193fb6
Vaibhav Pandey <vaibhavkpandey7@gmail.com>
2025-09-25 15:23:07 +0530

    added apis

--- src/shortVideo/routes/admin/index.js
+++ src/shortVideo/routes/admin/index.js
@@ -2,20 +2,29 @@ const Express = require('express');
 const { getTeam, getNetwork } = require('../../controllers/admin/tree.controller.admin');
 const { getUsersWithWatchTime, creditWatchTimeEarnings, resetAllWatchTime } = require('../../controllers/admin/credit.controller.admin');
 const { getSystemWallet, getSystemEarningLogs, transferFundsToPool, payoutWeeklyRewards } = require('../../controllers/admin/system.controller.admin');
+const { getPackagesWithUserCount } = require('../../controllers/admin/package.controller.admin');
 
 const shortVideoAdminRouter = Express.Router();
 
+
+//team & network
 shortVideoAdminRouter.get('/getteam', getTeam);
 shortVideoAdminRouter.get('/getnetwork', getNetwork);
 
+//watch time related
 shortVideoAdminRouter.get('/getuserswithwatchtime', getUsersWithWatchTime);
 shortVideoAdminRouter.put('/creditwatchtimeearnings',creditWatchTimeEarnings);
 shortVideoAdminRouter.put('/resetallwatchtime', resetAllWatchTime);
 
+//system wallet and weekly/monthly rewards
 shortVideoAdminRouter.get('/getsystemwallet', getSystemWallet);
 shortVideoAdminRouter.get('/getsystemearninglogs', getSystemEarningLogs);
 shortVideoAdminRouter.put('/transferfundstopool', transferFundsToPool);
 
 shortVideoAdminRouter.post('/payoutweeklyrewards', payoutWeeklyRewards);
+
+//package related
+shortVideoAdminRouter.get('/getpackageswithusercount', getPackagesWithUserCount);
+
 
 module.exports = shortVideoAdminRouter
