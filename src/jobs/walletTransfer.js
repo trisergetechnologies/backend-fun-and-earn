587be1a6136d13b1a5df3a0ef0858e3e2208dcd1
Vaibhav Pandey <vaibhavkpandey7@gmail.com>
2025-10-04 00:56:33 +0530

    added test

--- src/jobs/walletTransfer.js
+++ src/jobs/walletTransfer.js
@@ -4,17 +4,18 @@ const { default: axios } = require('axios');
 require('dotenv').config();
 
 
-cron.schedule('54 0 * * *', async () => {
+cron.schedule('0 6 * * *', async () => {
   try {
+
     console.log('📡 Running cron job at 6:00 AM IST');
+
     const ADMIN_EMAIL = process.env.ADMIN_EMAIL
-    console.log(ADMIN_EMAIL);
+    const URL = 'https://amp-api.mpdreams.in/api/v1/shortvideo/admin/transfershortvideotoecart';
+
     const admin = await User.findOne({email: ADMIN_EMAIL});
-    // const URL = 'https://amp-api.mpdreams.in/api/v1/shortvideo/admin/transfershortvideotoecart';
     const token = admin?.token;
-    const URL = 'https://amp-api.mpdreams.in/api/v1/shortvideo/admin/health-check';
-    const response = await axios.get(URL, {headers: {Authorization: `Bearer ${token}`}});
-    // const response = await axios.put(URL, {}, {headers: {Authorization: `Bearer ${token}`}});
+
+    const response = await axios.put(URL, {}, {headers: {Authorization: `Bearer ${token}`}});
 
     console.log('✅ API Response Success:', response.data.success);
   } catch (error) {
