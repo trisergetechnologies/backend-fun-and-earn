81aca05925f2dcb64d48fe62b41e11b7dc86bbb6
Vaibhav Pandey <vaibhavkpandey7@gmail.com>
2025-12-22 07:19:52 +0530

    orange public routes

--- src/eCart/routes/user/orangepg.routes.user.js
+++ src/eCart/routes/user/orangepg.routes.user.js
@@ -4,7 +4,7 @@
 // ================================================================
 
 const express = require('express');
-const { initiateOrangeSale, verifyOrangePaymentStatus, handleOrangeCallback, handlePaymentAdvice } = require('../../controllers/user/orangepg.controller.user');
+const { initiateOrangeSale, verifyOrangePaymentStatus, handleOrangeCallback, handlePaymentAdvice} = require('../../controllers/user/orangepg.controller.user');
 
 const orangePGRouter = express.Router();
 
--- src/routes/index.js
+++ src/routes/index.js
@@ -7,6 +7,7 @@ const authRouter = require('./auth.routes');
 //eCart router import
 const eCartRouter = require('../eCart/routes');
 const shortVideoRouter = require('../shortVideo/routes');
+const { handleOrangeCallback, handlePaymentAdvice } = require('../eCart/controllers/user/orangepg.controller.user');
 
 //Common Routes
 router.use('/auth', authRouter);
@@ -14,6 +15,13 @@ router.use('/auth', authRouter);
 //eCart Routes
 router.use('/ecart', eCartRouter);
 router.use('/shortvideo', shortVideoRouter);
+
+
+// Orange PG payment callback (Form POST from Orange PG after payment)
+orangePGRouter.post('/public/orange/callback', handleOrangeCallback);
+
+// Orange PG payment advice webhook (async notification from bank)
+orangePGRouter.post('/public/orange/paymentadvice', handlePaymentAdvice);
 
 
 
