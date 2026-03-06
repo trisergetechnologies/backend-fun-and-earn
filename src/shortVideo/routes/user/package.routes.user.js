bd6f7f47d1cad9fd00216d5d2c554d9ce149eff4
Vaibhav Pandey <vaibhavkpandey7@gmail.com>
2026-03-06 08:18:14 +0530

    routes won't throw error

--- src/shortVideo/routes/user/package.routes.user.js
+++ src/shortVideo/routes/user/package.routes.user.js
@@ -2,7 +2,13 @@ const express = require('express');
 const { purchasePackage, getPackages, getMyAchievement, getPackageOrders, getMyMonthlyAchievement, purchasePackageInternal } = require('../../controllers/user/package.controller.user');
 const userPackageRouter = express.Router();
 
-// userPackageRouter.post('/purchasepackage', purchasePackage);
+userPackageRouter.post('/purchasepackage', (req,res)=>{
+    return res.status(200).json({
+      success: true,
+      message: `Please Buy This from Dream Mart`,
+      data: null
+    });
+});
 userPackageRouter.get('/getpackage', getPackages);
 userPackageRouter.get('/getpackageorders', getPackageOrders);
 
