ef691d368bcfb2835cca39aea5fd1b25f3db4858
Vaibhav Pandey <vaibhavkpandey7@gmail.com>
2026-03-13 23:08:40 +0530

    network in basic to 2

--- src/config/packageDistributionConfig.js
+++ src/config/packageDistributionConfig.js
@@ -4,7 +4,7 @@ const PACKAGE_DISTRIBUTION_CONFIG = {
   Diamond: { networkRange: 20, teamLevels: 10 },
   Gold:    { networkRange: 10, teamLevels: 5 },
   Starter: { networkRange: 5,  teamLevels: 3 },
-  Basic:   { networkRange: 3,  teamLevels: 1 },
+  Basic:   { networkRange: 2,  teamLevels: 1 },
 };
 
 const DEFAULT_CONFIG = { networkRange: 3, teamLevels: 1 };
